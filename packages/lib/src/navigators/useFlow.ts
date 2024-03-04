d20d24ce5306da3a9c038512f732d6f3e52e4953
Charlotte Isambert <charlotte.isambert@gmail.com>
2024-03-04 17:22:58 +0100

    Wait for re-renders The previous method with useEffect was not reliable, the callback wasn't triggered every time

--- packages/lib/src/navigators/useFlow.ts
+++ packages/lib/src/navigators/useFlow.ts
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useContext } from "react";
+import { useContext } from "react";
 import { FlowContext } from "./FlowContext";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { FlowActionHelpers } from "../routers/FlowRouter";
@@ -22,9 +22,6 @@ export const useFlow = () => {
 const useFlowHelpers = () => {
   const { goToNextStep, goToPreviousStep, quitFlow } =
     useNavigation<FlowActionHelpers<ParamListBase>>();
-
-  const { waitForRerender } = useWaitForRerender();
-
   const asyncGoToNextStep = () => waitForRerender(goToNextStep);
   const asyncGoToPreviousStep = () => waitForRerender(goToPreviousStep);
 
@@ -42,21 +39,6 @@ const useFlowHelpers = () => {
  *
  * See this issue for more details: https://github.com/react-navigation/react-navigation/discussions/11839#discussioncomment-8549314
  * */
-const useWaitForRerender = () => {
-  const [runAfterRendered, setRunAfterRendered] = useState(false);
-  const callback = useRef(() => {});
-
-  useEffect(() => {
-    if (runAfterRendered) {
-      callback.current?.();
-      setRunAfterRendered(false);
-    }
-  }, [runAfterRendered]);
-
-  const waitForRerender = (newCallback: () => void) => {
-    callback.current = newCallback;
-    setRunAfterRendered(true);
-  };
-
-  return { waitForRerender };
+const waitForRerender = (callback: () => void) => {
+  setTimeout(callback, 0);
 };
